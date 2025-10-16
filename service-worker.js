b3392af0530b0c5a84ebf8522de346f4e869c9e9
heinz0099-jpg <heinz0099@gmail.com>
2025-10-16 23:33:45 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,37 +9,26 @@ const URLS_TO_CACHE = [
   './icon-512.png'
 ];
 
-// Install Event: Dateien cachen
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(URLS_TO_CACHE))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)).then(() => self.skipWaiting())
   );
 });
 
-// Activate Event: alten Cache löschen
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
     ).then(() => self.clients.claim())
   );
 });
 
-// Fetch Event: zuerst aus Cache, sonst Netzwerk
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(cached => cached || fetch(event.request))
-      .catch(() => {
-        // Optional: Offline-Fallback für iOS/Android
-        if (event.request.destination === 'document') {
-          return caches.match('./index.html');
-        }
-      })
+    caches.match(event.request).then(resp => resp || fetch(event.request)).catch(() => {
+      if (event.request.destination === 'document') {
+        return caches.match('./index.html');
+      }
+    })
   );
 });
